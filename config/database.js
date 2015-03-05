1bf401ae844b80dc562109652e2268d55968373f
alvartabe <alvartabe777@gmail.com>
2015-03-05 09:30:18 -0600

    test

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+	'url' : 'mongodb://lawepa:12345@ds047911.mongolab.com:47911/nacion' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
